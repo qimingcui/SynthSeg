909034765057843c0fc559a792d0c4b1b9fbc96b
gavincui19 <qcui@berkeley.edu>
2025-04-30 15:53:55 -0700

    new file:   scripts/custom/5-generation_advanced.py 	new file:   scripts/custom/quant_test.ipynb

--- scripts/tutorials/1-generation_visualisation.py
+++ scripts/tutorials/1-generation_visualisation.py
@@ -18,15 +18,27 @@ distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, e
 implied. See the License for the specific language governing permissions and limitations under the
 License.
 """
-
+# %%
+import sys, os
+from pathlib import Path
+sys.path.append(os.path.join( os.getcwd(), '../../'))
 
 from ext.lab2im import utils
 from SynthSeg.brain_generator import BrainGenerator
 
+# %%
+import tensorflow as tf
+print(tf.__version__)  # Should print 2.2.0
+
+from tensorflow import keras
+print(keras.__version__)  # Should match tf.__version__
+# %%
+
+
 # generate an image from the label map.
-brain_generator = BrainGenerator('../../data/training_label_maps/training_seg_01.nii.gz')
+brain_generator = BrainGenerator('/mnt/hdd0/MRI_data/ADNI/synthseg_brain_generator_test/label_map.nii')
 im, lab = brain_generator.generate_brain()
 
 # save output image and label map under SynthSeg/generated_examples
-utils.save_volume(im, brain_generator.aff, brain_generator.header, './outputs_tutorial_1/image.nii.gz')
-utils.save_volume(lab, brain_generator.aff, brain_generator.header, './outputs_tutorial_1/labels.nii.gz')
+utils.save_volume(im, brain_generator.aff, brain_generator.header, '/mnt/hdd0/MRI_data/ADNI/synthseg_brain_generator_test//outputs_tutorial_1/image.nii.gz')
+utils.save_volume(lab, brain_generator.aff, brain_generator.header, '/mnt/hdd0/MRI_data/ADNI/synthseg_brain_generator_test//outputs_tutorial_1/labels.nii.gz')
